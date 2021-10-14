2201ae72f0eb44d84fbc5ffb6afbf9b773e118e0
Amilcar Sanchez <amillcar.sanchez@wizeline.com>
2021-10-14 12:50:34 -0500

    CustomerForm tests

---
+++ test/domManipulators.js
@@ -0,0 +1,9 @@
+import ReactDom from "react-dom";
+
+export const createContainer = () => {
+  const container = document.createElement("div");
+  return {
+    render: (component) => ReactDom.render(component, container),
+    container,
+  };
+};
